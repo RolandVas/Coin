4e201071f716dff61be9fbd299907966ddab0cd9
RolandVas <rolandvas@hotmail.com>
2023-01-16 20:52:46 +0100

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'coin-e468d',
+    appId: '1:1025759633012:web:524821c08df08ba57b7b21',
+    storageBucket: 'coin-e468d.appspot.com',
+    apiKey: 'AIzaSyAk_YjYZIkTPWRKiWOHWZ_lPhfEOEVe_g8',
+    authDomain: 'coin-e468d.firebaseapp.com',
+    messagingSenderId: '1025759633012',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,22 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'coin-e468d',
+    appId: '1:1025759633012:web:524821c08df08ba57b7b21',
+    storageBucket: 'coin-e468d.appspot.com',
+    apiKey: 'AIzaSyAk_YjYZIkTPWRKiWOHWZ_lPhfEOEVe_g8',
+    authDomain: 'coin-e468d.firebaseapp.com',
+    messagingSenderId: '1025759633012',
+  },
+  firebaseConfig: {
+    apiKey: "AIzaSyAk_YjYZIkTPWRKiWOHWZ_lPhfEOEVe_g8",
+    authDomain: "coin-e468d.firebaseapp.com",
+    projectId: "coin-e468d",
+    storageBucket: "coin-e468d.appspot.com",
+    messagingSenderId: "1025759633012",
+    appId: "1:1025759633012:web:524821c08df08ba57b7b21"
+  },
   production: false
 };
 
